c2545f8957613e9ffbd2a7e3990c0f79e24911e5
suguannan <guannan.su@net263.com>
2017-02-21 18:04:04 +0800

    first commit

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="goods">
+Im goods
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="header">
+    我是header
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+
+    };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
